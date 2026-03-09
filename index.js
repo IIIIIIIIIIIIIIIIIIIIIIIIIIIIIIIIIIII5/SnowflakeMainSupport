45b0874ad6d71c3a2f74fc8ed839a63446315778
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2026-03-09 12:42:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,20 +31,12 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: [] }
     );
     console.log("Commands cleared successfully.");
 
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
     console.log("Commands registered successfully.");
